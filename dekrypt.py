529bc9903ce7138f4fdbad9544768d83bcc869f1
PEREIRA <alex.was.pereira@gmail.com>
2014-08-07 19:18:46 +0200

    adding support of md5 and sha1 coming in finalization

--- dekrypt.py
+++ dekrypt.py
@@ -21,36 +21,89 @@ class bcolors:
     ENDC = '\033[0m'
 
 ##
+# output - rendering the messages for the app
+# @def output(successType, msg)
+# @param successType The type of the message (0 : normal, 1: success, 2: fail)
+# @param msg String containing the message to display
+
+def output(successType, msg):
+	if(successType == 0):
+		out = "[*] " + msg
+	elif(successType == 1):
+		out = bcolors.OKGREEN + "[+] "+ msg + bcolors.ENDC
+	elif(successType == 2):
+		out = bcolors.FAIL + "[-] "+ msg + bcolors.ENDC
+	return out
+
+##
+# un_crypt - decryption process with crypt
+# @def un_crypt(cryptPass, word)
+# @param cryptPass The crypted pass to decode.
+# @param word The dictionnary word to compare.
+
+def un_crypt(cryptPass, word):
+	salt = cryptPass[0:2]
+	cryptWord = crypt.crypt(word, salt)
+	if(cryptWord == cryptPass):
+		return word
+	return False
+
+##
+# un_hashl - decryption process with hashlib
+# @def un_hashl(cryptPass, word)
+# @param cryptPass The crypted pass to decode.
+# @param word The dictionnary word to compare.
+
+def un_hashl(cryptPass, word):
+	f = hashlib.md5()
+	f.update(word)
+	hashWord_md5 = f.hexdigest()
+	print hashWord_md5
+	print cryptPass
+	if(hashWord_md5 == cryptPass):
+		print output(1, "Found Type : Looks like a md5 hash...")	
+		return word
+	u = hashlib.sha1()
+	u.update(word)
+	hashWord_sha1 = u.hexdigest()
+	if(hashWord_sha1 == cryptPass):
+		print output(1, "Found Type : Looks like a sha1 hash...")	
+		return word
+	return False
+
+##
 # dekrypt - main dekrypt function, launching the decryption process
 # @def dekrypt(cryptPass)
 # @param cryptPass The crypted pass to decode.
 
 def dekrypt(cryptPass):
-	salt = cryptPass[0:2]
 	dictFile = open('dictionnary.txt', 'r')
 	for word in dictFile.readlines():
 		word = word.strip('\n')
-		cryptWord = crypt.crypt(word, salt)
-		if(cryptWord == cryptPass):
-			print bcolors.OKGREEN + "[+] Found Password : "+ word +"\n" + bcolors.ENDC
+		if(un_crypt(cryptPass, word) != False):
+			print output(1, "Found Password : " + word +"\n")
 			return
-	print bcolors.FAIL + "[-] Password Not Found. \n" + bcolors.ENDC
-	return
+		elif(un_hashl(cryptPass, word) != False):
+			print output(1, "Found Password : " + word +"\n")
+			return
+	print output(2, "Password Not Found.\n")
+	return			
 
 ##
 # main - main initializer
 # @def main()
 
 def main():
-	passFile = open('password.txt')
+	passFile = open('password.txt', 'r')
 	for line in passFile.readlines():
 		if ":" in line:
 			user = line.split(':')[0]
 			cryptPass = line.split(':')[1].strip(' ').strip('\n')
-			print "[*] Cracking Password "+ cryptPass +" For User : "+ user
+			print output(0, "Cracking Password "+ cryptPass + " for User " + user)
 			dekrypt(cryptPass)
 		else:
 			cryptPass = line.strip(' ')
+			print output(0, "Cracking Password "+ cryptPass)
 			dekrypt(cryptPass)
 
 if __name__ == "__main__":
